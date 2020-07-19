e68666f12f81ea3cf29c88372e806617456d2f98
Dmitriy Datskovskiy <datskovskiydn@altarix.ru>
2020-07-19 11:31:43 +0400

    apache

---
+++ manager/bin/app.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+echo 'Hello, World!1' . PHP_EOL;
---
+++ manager/public/index.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+echo 'Hello, World!2';
+echo '<html lang="en">
+<head>
+  <title>App</title>
+  <meta charset="UTF-8" />
+</head>
+<body>
+  <h1>Hello, World!</h1>
+</body>
+</html>';
