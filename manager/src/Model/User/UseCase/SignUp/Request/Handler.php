6fae02cd23b7a77d6bf0e09ba3545726017b9101
Дмитрий Дацковский <dadzki@yandex.ru>
2020-08-19 12:33:44 +0400

    sign up and confirm

--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -63,8 +63,8 @@ class Handler
         );
 
         $this->users->add($user);
-        $this->sender->send($email, $token);
-
         $this->flusher->flush();
+
+        $this->sender->send($email, $token);
     }
 }
